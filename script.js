db50d34cfc6af3504fe5b3af860216781b1703b5
John Kluse <j.kluse@gmail.com>
2023-04-05 13:00:27 -0600

    Completed Flashing Checkerboard

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
-const container = document.createElement("div");
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
-container.style.width = "1000px";
-container.style.height = "1000px";
-document.body.append(container);
+let container;
 
-let toggleBlack = false;
-let count = 0;
-let rgb1 = [180, 240, 240];
-let rgb2 = [0, 240, 240];
-
-for (let i = 0; i < 64; i++) {
-	count++;
-	const tile = document.createElement("div");
-	tile.style.width = "12.5%";
-	tile.style.height = "12.5%";
-
-	//update gradient
-	rgb1[1] -= 10;
-	rgb2[2] -= 10;
-
-	if (toggleBlack) {
-		tile.style.background = `rgb(180, ${rgb1[1]}, ${rgb1[2]})`;
-	} else {
-		tile.style.background = `rgb(0, ${rgb1[1]}, ${rgb1[2]})`;
-	}
-
-	// toggle background
-	toggleBlack = toggleBlack ? false : true;
-
-	if (count === 8) {
-		toggleBlack = toggleBlack ? false : true;
-		count = 0;
-	}
-
-	container.append(tile);
+function createContainer() {
+	container = document.createElement("div");
+	container.style.display = "flex";
+	container.style.flexWrap = "wrap";
+	container.style.width = "1000px";
+	container.style.height = "1000px";
+	document.body.append(container);
 }
+
+function flash() {
+	if (container) container.remove();
+	createContainer();
+	for (let i = 0; i < 64; i++) {
+		const randomR = Math.floor(Math.random() * 256);
+		const randomG = Math.floor(Math.random() * 256);
+		const randomB = Math.floor(Math.random() * 256);
+
+		const tile = document.createElement("div");
+		tile.style.width = "12.5%";
+		tile.style.height = "12.5%";
+
+		tile.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+
+		container.append(tile);
+	}
+}
+
+setInterval(flash, 2000);
+// flash();
